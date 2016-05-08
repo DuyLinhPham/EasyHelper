9b044e6ace31ee2fba2fc24e59e6ee1125daaa06
linh pham <duylinh191@gmail.com>
2016-05-08 15:28:59 +0700

    Add class

---
+++ EasyHelper/EmailHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyHelper
+{
+    /// <summary>
+    /// It supports for sending email
+    /// </summary>
+    public sealed class EmailHelper
+    {
+        //TODO: Implement
+    }
+}
---
+++ EasyHelper/Security.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EasyHelper
+{
+    /// <summary>
+    /// It supports Encrypt and Decrypt string
+    /// </summary>
+    public sealed class Security
+    {
+        public static string Key { get; set; }
+
+        /// <summary>
+        /// Encrypt string
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>string</returns>
+        public static string EncryptString(string message)
+        {
+            byte[] results;
+            System.Text.UTF8Encoding utf8 = new System.Text.UTF8Encoding();
+
+            MD5CryptoServiceProvider hashProvider = new MD5CryptoServiceProvider();
+            byte[] tdesKey = hashProvider.ComputeHash(utf8.GetBytes(Key));
+
+
+            TripleDESCryptoServiceProvider tdesAlgorithm = new TripleDESCryptoServiceProvider
+            {
+                Key = tdesKey,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+
+            byte[] dataToEncrypt = utf8.GetBytes(message);
+
+            try
+            {
+                ICryptoTransform encryptor = tdesAlgorithm.CreateEncryptor();
+                results = encryptor.TransformFinalBlock(dataToEncrypt, 0, dataToEncrypt.Length);
+            }
+            finally
+            {
+
+                tdesAlgorithm.Clear();
+                hashProvider.Clear();
+            }
+
+            return Convert.ToBase64String(results);
+        }
+
+        /// <summary>
+        /// Decrypt string
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>string</returns>
+        public static string DecryptString(string message)
+        {
+            byte[] results;
+            System.Text.UTF8Encoding utf8 = new System.Text.UTF8Encoding();
+
+            MD5CryptoServiceProvider hashProvider = new MD5CryptoServiceProvider();
+            byte[] tdesKey = hashProvider.ComputeHash(utf8.GetBytes(Key));
+
+            TripleDESCryptoServiceProvider tdesAlgorithm = new TripleDESCryptoServiceProvider
+            {
+                Key = tdesKey,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+
+            byte[] dataToDecrypt = Convert.FromBase64String(message);
+
+            try
+            {
+                ICryptoTransform decryptor = tdesAlgorithm.CreateDecryptor();
+                results = decryptor.TransformFinalBlock(dataToDecrypt, 0, dataToDecrypt.Length);
+            }
+            finally
+            {
+
+                tdesAlgorithm.Clear();
+                hashProvider.Clear();
+            }
+
+
+            return utf8.GetString(results);
+        }
+    }
+}
